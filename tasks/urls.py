473c3543305317bced9dbe5ade9444633b02d85a
MOHAMEDSAMEER78 <mohamedsameermms@gmail.com>
2025-04-23 22:47:08 +0530

    Updating Urls for endpoints

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path
 from rest_framework import routers
 from tasks import views
 
@@ -6,3 +7,6 @@ router = routers.DefaultRouter()
 router.register(r"tasks", views.TaskView, "task")
 
 urlpatterns = router.urls
+urlpatterns += [
+    path('tasks/completed/', views.TaskView.as_view({'get': 'completed'}), name='task-completed'),
+]
