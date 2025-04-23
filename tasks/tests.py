c58962ad11e4ab4e9cfa4fe1b2d41a44119a9d7f
Sairam-Panuku <sairampanuku123@gmail.com>
2025-04-24 02:14:16 +0530

    Add README.md and update project details

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from .models import Task
 
-# Create your tests here.
+class TaskTests(APITestCase):
+    def test_create_task(self):
+        url = reverse('task-list')
+        data = {'title': 'Test', 'description': 'Description'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
