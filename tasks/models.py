e35a9a11d506899dea98d0a17c3540875c19b008
MOHAMEDSAMEER78 <mohamedsameermms@gmail.com>
2025-04-24 13:48:36 +0530

    Revert "Sample"
    This reverts commit 321227f1e6356984d691c95ed89c4f1da2f7bbc9.

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone
 
 
 # Create your models here.
@@ -7,7 +6,6 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False, blank=True, null=True)
-    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
